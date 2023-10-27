0658de923d06472dceacefa5652b4b46dc1676bb
Arthur Castro <arthurcastro078@gmail.com>
2023-10-27 15:04:39 -0300

    spcript python

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 # Abrir o arquivo L3.log em modo de leitura
 def openLog(f):
@@ -27,7 +28,44 @@ def lerResultados(indicador, out):
                         resultados[region_name] = r
     return resultados
 
-n = {64, 100, 128,  200, 256, 512, 600, 900, 1024, 2000, 2048, 3000, 4000}
+def escreverResultados(resultados, diretorio_saida):
+    # Iterar pelos indicadores
+    for grupo, asd in resultados.items():
+        for indicador, valores in asd.items():
+            # Criar o nome do arquivo de saída com base no indicador
+
+            os.makedirs(diretorio_saida, exist_ok=True)
+
+            if len(asd) > 1:
+                nome_arquivo = f"{diretorio_saida}/{indicador.replace(' ', '-').replace('/', '')}_resultados.csv"
+            else:
+                nome_arquivo = f"{diretorio_saida}/{grupo}_resultados.csv"
+            
+            # Abrir o arquivo de saída em modo de escrita
+            with open(nome_arquivo, "w", newline="") as csvfile:
+                # Cria um escritor CSV
+                csv_writer = csv.writer(csvfile)
+                
+                # Escreve o cabeçalho do CSV com os nomes das regiões
+                cabecalho = ["n"]
+                if len(valores) > 0:
+                    i = list(valores.keys())[0]
+                    for regiao in valores[i].keys():
+                        cabecalho.append(regiao)
+                csv_writer.writerow(cabecalho)
+
+
+                # Escreve os dados no CSV
+                for n, r in valores.items():
+                    linha = [n]
+                    for valor in r.values():
+                        linha.append(valor)
+                    csv_writer.writerow(linha)
+
+            print(f"Valores do indicador {indicador} foram escritos no arquivo {nome_arquivo}")
+
+
+n = [64, 100, 128,  200, 256, 512, 600, 900, 1024, 2000, 2048, 3000, 4000]
 
 ind = {
     "L3": ["L3 bandwidth [MBytes/s]"],
@@ -41,22 +79,20 @@ dir = "resultados/"
 resultados = {}
 
 for o, i in ind.items():
+    resultados[o] = {}
+    for j in i:
+        resultados[o][j] = {}
+
+for o, i in ind.items():
+    k = 0
     for m in n:
-        f = dir + o + ".log"
+        f = dir + o + "-" + str(m) + ".log"
         if not os.path.isfile(f):
             print(f"O arquivo {f} não foi encontrado.")
         else:
             output = openLog(f)
-            resultados[o] = {}
             for j in i:
-                resultados[o][j] = lerResultados(j, output)
+                resultados[o][j][str(m)] = lerResultados(j, output)
 
-for i, r in resultados.items():
-    print(i)
-    j = 0
-    for res in r:
-        for region, valor in res.items():
-            print(f"Região: {region}")
-            print(f"{ind[i][j]}: {valor}")
-            print()
-        j = j + 1
+
+escreverResultados(resultados, "resultados_finais")
